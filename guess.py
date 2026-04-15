2d119e6d50d2b33f5c1b72f17b1860d0f76d8152
Smit Dighe <smitdighe@gmail.com>
2026-04-15 23:12:10 +0530

    Fix win message in guessing game to improve clarity

--- guess.py
+++ guess.py
@@ -4,4 +4,4 @@ guess = int(input("Guess number (1-50): "))
 while guess != n:
     print("Too high!" if guess > n else "Too low!")
     guess = int(input("Try again: "))
-print("You won!")
+print("You guessed correctly!")
